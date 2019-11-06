562a1537ec0ebb84a5f71f6a57f902529aa9f571
ziyingwong <zy_508@hotmail.com>
2019-11-06 17:36:28 +0800

    v2

---
+++ app/src/main/java/com/example/superWiserTVV2/halfway/ViewMoreActivity.kt
@@ -0,0 +1,12 @@
+package com.example.superWiserTVV2.halfway
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.superWiserTVV2.R
+
+class ViewMoreActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.viewmore_fragment)
+    }
+}
